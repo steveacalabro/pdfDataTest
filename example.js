bf82eaa71be369968d853b2e20cd2f482e4531f0
Stephen A Calabro <steve.a.calabro@gmail.com>
2017-08-01 22:45:25 -0700

    Added module exports for node package

---
+++ example.js
@@ -0,0 +1,3 @@
+var pdfParse = require('./index');
+
+pdfParse.parsePdfFile("pdfData.pdf");
--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 var pdf_table_extractor = require("pdf-table-extractor");
 var fs = require('fs');
 
-function searchAttribute(data) {
-  console.log(data.filter(function(x) {
-    return x.sic === 3291;
-  }));
-}
-
 function newLineToArray(input) {
   if (input.indexOf('\n') > -1) {
     // Contains a newline Char
@@ -27,45 +21,48 @@ function success(result) {
         for (let k = 0; k < newLineToArray(cur[3]).length; k++) {
           const row = {
             'isoDescription': cur[0],
-            'isoCGL': cur[1],
-            'sic': newLineToArray(cur[2])[k],
-            'naics': newLineToArray(cur[3])[k],
+            'isoCGL': parseInt(cur[1]),
+            'sic': parseInt(newLineToArray(cur[2])[k]),
+            'naics': parseInt(newLineToArray(cur[3])[k]),
             'generalDescription': newLineToArray(cur[4])[k],
-            'ncci': newLineToArray(cur[5])[k],
-            'caWC': newLineToArray(cur[6])[k],
-            'deWC': newLineToArray(cur[7])[k],
-            'miWC': newLineToArray(cur[8])[k],
-            'njWC': newLineToArray(cur[9])[k],
-            'nyWC': newLineToArray(cur[10])[k],
-            'paWC': newLineToArray(cur[11])[k],
-            'txWC': newLineToArray(cur[12])[k]
+            'ncci': parseInt(newLineToArray(cur[5])[k]),
+            'caWC': parseInt(newLineToArray(cur[6])[k]),
+            'deWC': parseInt(newLineToArray(cur[7])[k]),
+            'miWC': parseInt(newLineToArray(cur[8])[k]),
+            'njWC': parseInt(newLineToArray(cur[9])[k]),
+            'nyWC': parseInt(newLineToArray(cur[10])[k]),
+            'paWC': parseInt(newLineToArray(cur[11])[k]),
+            'txWC': parseInt(newLineToArray(cur[12])[k])
           };
           parsedData.push(row);
         }
       } else {
         const row = {
           'isoDescription': cur[0],
-          'isoCGL': cur[1],
-          'sic': cur[2],
-          'naics': cur[3],
+          'isoCGL': parseInt(cur[1]),
+          'sic': parseInt(cur[2]),
+          'naics': parseInt(cur[3]),
           'generalDescription': cur[4],
-          'ncci': cur[5],
-          'caWC': cur[6],
-          'deWC': cur[7],
-          'miWC': cur[8],
-          'njWC': cur[9],
-          'nyWC': cur[10],
-          'paWC': cur[11],
-          'txWC': cur[12]
+          'ncci': parseInt(cur[5]),
+          'caWC': parseInt(cur[6]),
+          'deWC': parseInt(cur[7]),
+          'miWC': parseInt(cur[8]),
+          'njWC': parseInt(cur[9]),
+          'nyWC': parseInt(cur[10]),
+          'paWC': parseInt(cur[11]),
+          'txWC': parseInt(cur[12])
         };
         parsedData.push(row);
       }
     }
   }
 
-  fs.writeFile('./output2.json', JSON.stringify(parsedData), function(err) {
-    if (err)
+  fs.writeFile('./output.json', JSON.stringify(parsedData), function(err) {
+    if (err) {
       return console.log(err);
+    }
+
+    console.log("Saved File");
   });
 }
 
@@ -74,4 +71,16 @@ function error(err) {
   console.error('Error: ' + err);
 }
 
-pdf_table_extractor("pdfData.pdf", success, error);
+module.exports = {
+  parsePdfFile: function(fileName) {
+    pdf_table_extractor(fileName, success, error); // "pdfData.pdf""
+  },
+}
+
+/*
+export function searchAttribute(data, key, searchTerm) {
+  return data.filter(function(x) {
+    return x[key] === searchTerm;
+  });
+}
+*/
